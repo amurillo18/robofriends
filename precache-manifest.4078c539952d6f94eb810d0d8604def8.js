6de972e86e57cd9d35053ed3f6d33a4398c88f04
April Murillo <afreyde@wgu.edu>
2022-04-26 13:26:52 -0700

    Updates

---
+++ precache-manifest.4078c539952d6f94eb810d0d8604def8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cda0d99a8c8e95f8d23",
+    "url": "/robofriends/static/css/main.544fec51.chunk.css"
+  },
+  {
+    "revision": "9cda0d99a8c8e95f8d23",
+    "url": "/robofriends/static/js/main.9cda0d99.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "87f5f78d2d0af8d57672",
+    "url": "/robofriends/static/css/2.251ac8e4.chunk.css"
+  },
+  {
+    "revision": "87f5f78d2d0af8d57672",
+    "url": "/robofriends/static/js/2.87f5f78d.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "02764f12d38cd3efc869df5afbc364c3",
+    "url": "/robofriends/index.html"
+  }
+];
